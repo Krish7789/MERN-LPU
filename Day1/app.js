3a8fc924541a57476344aa652c0178243f137d8d
Krish7789 <83880418+Krish7789@users.noreply.github.com>
2025-06-16 13:32:27 +0530

    day1-part1

---
+++ Day1/app.js
@@ -0,0 +1,24 @@
+window.console.log("Hello from JS!");
+
+//let, cont OR var
+
+// let age=30;
+// cont year= 2025;
+// // var block=22;
+// console.age(block);
+// console.log(age);
+
+// var age=22;  //declaration and initialization
+// console.log(age);
+
+// var age=44;  //re-declaration
+//console.log(age);
+
+
+let age = 22;
+
+console.log(age);
+
+age = 24;
+
+console.log(age);
