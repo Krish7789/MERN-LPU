d4171a2e7c4cad9d7178784506c9ea88228f368d
Krish7789 <83880418+Krish7789@users.noreply.github.com>
2025-06-17 10:22:00 +0530

    Objects and Arrays

---
+++ Day2/arrays_and _objects/app.js
@@ -0,0 +1,33 @@
+// const cse018 = {
+//     name: "Krish",
+//     address: "Jalandhar, PB",
+//     bloodGroup: "B+",
+// };
+
+// console.log(cse018);
+
+// const cse75 = {
+//     name: "Shrinivas",
+//     address: "Hyderabad, TS",
+//     bloodGroup: "O+",
+// };
+
+// console.log(cse75);
+
+const cse018 = {
+    name: "Krish",
+    address: "Jalandhar, PB",
+    bloodGroup: "B+",
+};
+
+// READ THE VALUE OF PROPERTY
+const studentName = cse018.name;
+console.log("studentName:", studentName);
+
+// UPDATE THE VALUE OF PROPERTY
+cse018.bloodGroup = "A+";
+
+//ADD A KEY-VALUE PAIR
+cse018.college = "LPU";
+
+console.log("cse018:", cse018);
