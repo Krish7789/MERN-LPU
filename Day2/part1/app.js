f3bf9512b346f9c665c584563813aaea2ccc921a
Krish7789 <83880418+Krish7789@users.noreply.github.com>
2025-06-17 09:26:58 +0530

    Initial commit for Day2 part1

---
+++ Day2/part1/app.js
@@ -0,0 +1,19 @@
+window.console.log("hello");
+const printBill = () => {
+    console.log("------------");
+    console.log("Rs.", txt);
+    console.log("------------");
+};
+
+cosnt calculateBillAmountForFood = (price) => {
+    return price * 1.05;
+};
+
+const calculateBillAmountForFood = (price) => {
+    return price * 1.12;
+};
+
+const calculateBillAmountForFood = (price) => {
+    return price * 1.2;
+};
+
