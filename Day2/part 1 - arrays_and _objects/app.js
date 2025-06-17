febd8881ce2bc3da6972e86cf3d677411aab2ba0
Krish7789 <83880418+Krish7789@users.noreply.github.com>
2025-06-17 11:47:58 +0530

    Arrays & Objects

--- Day2/part 1 - arrays_and _objects/app.js
+++ Day2/part 1 - arrays_and _objects/app.js
@@ -1,36 +1,109 @@
+// // const cse018 = {
+// //     name: "Krish",
+// //     address: "Jalandhar, PB",
+// //     bloodGroup: "B+",
+// // };
+
+// // console.log(cse018);
+
+// // const cse75 = {
+// //     name: "Shrinivas",
+// //     address: "Hyderabad, TS",
+// //     bloodGroup: "O+",
+// // };
+
+// // console.log(cse75);
+
 // const cse018 = {
-//     name: "Krish",
+//     name: "Krish Thapa",
 //     address: "Jalandhar, PB",
 //     bloodGroup: "B+",
 // };
 
-// console.log(cse018);
+// // READ THE VALUE OF PROPERTY
+// const studentName = cse018.name;
+// console.log("studentName:", studentName);
 
-// const cse75 = {
-//     name: "Shrinivas",
-//     address: "Hyderabad, TS",
-//     bloodGroup: "O+",
+// // UPDATE THE VALUE OF PROPERTY
+// cse018.bloodGroup = "A-";
+
+// //ADD A KEY-VALUE PAIR
+// cse018.college = "LPU";
+
+// //DELETE
+// delete cse018.address;
+
+// console.log("cse018:", cse018);
+
+// const person = {
+//     name: "Krish",
+//     height: 1.7, //metres
+//     weight: 78, //kg
+//     college: "LPU",
+//     rollNumber: 15,
+//     getBMI: function(){
+
+//         console.log(this.height, this.weight); //height & weight
+//     },
+// }
+
+// person.getBMI();
+
+// person.weight = 90;
+
+// person.getBMI();
+
+// const allKeys = Object.keys(person);
+// console.log("allKeys:", allKeys);
+// const allValues = Object.values(person);
+// console.log("allKeys:", allKeys);
+// const allEntries = Object.entries(person);
+// console.log("allKeys:", allKeys);
+
+
+//-------------------------Dynamic Value Access----------------------------
+// const person = {
+//     name: "Krish",
+//     height: 1.7, //metres
+//     weight: 78, //kg
+//     college: "LPU",
+//     rollNumber: 15,
 // };
 
-// console.log(cse75);
+// //READ
+// const whichkey = prompt("What value do you want?");
+// console.log("whichkey:", whichkey);
+// const clg = person[whichkey];
+// console.log(clg);
 
-const cse018 = {
-    name: "Krish Thapa",
-    address: "Jalandhar, PB",
-    bloodGroup: "B+",
+//----------------------------------
+
+// const person1 = {
+//     name: "Krish",
+//     height: 1.7, //metres
+//     weight: 78, //kg
+//     college: "LPU",
+//     rollNumber: 15,
+// };
+
+// const person2 = person1
+
+// person2.name = "Raj";
+
+// console.log("person1", person1);
+// console.log("person2", person2);
+
+//------------------------------
+
+const person1 = {
+    name: "Krish",
+    height: 1.7, //metres
+    weight: 78, //kg
+    college: "LPU",
+    rollNumber: 15,
 };
 
-// READ THE VALUE OF PROPERTY
-const studentName = cse018.name;
-console.log("studentName:", studentName);
+//-----------De-structuring
 
-// UPDATE THE VALUE OF PROPERTY
-cse018.bloodGroup = "A-";
-
-//ADD A KEY-VALUE PAIR
-cse018.college = "LPU";
-
-//DELETE
-delete cse018.address;
-
-console.log("cse018:", cse018);
+const { weight, college } = person1;
+console.log(": --->:", weight, college);
