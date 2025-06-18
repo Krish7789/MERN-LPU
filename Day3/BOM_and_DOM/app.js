9dfeab805d54a16e0e8ff35dc1288b57d0c3b6cb
Krish7789 <83880418+Krish7789@users.noreply.github.com>
2025-06-18 11:25:57 +0530

    initial structure for bom and dom

---
+++ Day3/BOM_and_DOM/app.js
@@ -0,0 +1,5 @@
+// console.dir(window.document);
+// 1. document.getElement_s_ByTagName --> [] (iterable) (HTML Collection)
+
+const headings = document.getElementsByTagName("h1");
+console.log("headings", headings);
